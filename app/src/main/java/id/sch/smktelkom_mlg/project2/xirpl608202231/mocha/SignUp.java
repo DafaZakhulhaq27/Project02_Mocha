1e73007def48e7c97fa5efa8eaff0a0c2ce5661e
Dafa Zakhulhaq <dafa27890@gmail.com>
2017-03-19 20:30:55 +0700

    Pembuatan Logout User

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl608202231/mocha/SignUp.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl608202231/mocha/SignUp.java
@@ -1,12 +1,15 @@
 package id.sch.smktelkom_mlg.project2.xirpl608202231.mocha;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,11 +20,11 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
     FirebaseAuth firebaseAuth;
-
     EditText user;
     EditText pass;
     Button signup;
     ProgressDialog progressDialog;
+    TextView ready;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +33,42 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //lek user gak ana
+        if (firebaseAuth.getCurrentUser() != null) {
+            //tutup activity
+            finish();
+
+            //buka activity anyar
+            startActivity(new Intent(getApplicationContext(), Main.class));
+        }
+
         user = (EditText) findViewById(R.id.etuser);
         pass = (EditText) findViewById(R.id.etpass);
         signup = (Button) findViewById(R.id.etup);
+        ready = (TextView) findViewById(R.id.tvready);
 
         progressDialog = new ProgressDialog(this);
 
         signup.setOnClickListener(this);
+        ready.setOnClickListener(this);
 
     }
 
+    //buat daftar
     private void daftar() {
 
         String user1 = user.getText().toString().trim();
         String pass1 = pass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(user1)) {
+            Toast.makeText(this, "Tolong masukan username(email)", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(pass1)) {
+            Toast.makeText(this, "Tolong masukan password", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         progressDialog.setMessage("Proses Mendaftar...");
         progressDialog.show();
@@ -55,6 +80,9 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 if (task.isSuccessful()) {
 
                     Toast.makeText(SignUp.this, "Berhasil Mendaftar", Toast.LENGTH_LONG).show();
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), Main.class));
+
 
 
                 } else {
@@ -72,6 +100,13 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
-        daftar();
+        if (view == signup) {
+            daftar();
+        }
+        if (view == ready) {
+
+            startActivity(new Intent(this, Login.class));
+        }
+
     }
 }
